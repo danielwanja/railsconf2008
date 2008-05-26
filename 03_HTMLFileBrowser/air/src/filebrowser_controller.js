e573fd60d5a251a8afec6d3ebca52f7515e66e21
Tony Hillerson <tony.hillerson@effectiveui.com>
2008-05-26 00:26:37 -0600

    reworked file browser as a 'file size cloud' browser. browse directories and see a 'cloud' of files by size.

--- 03_HTMLFileBrowser/air/src/filebrowser_controller.js
+++ 03_HTMLFileBrowser/air/src/filebrowser_controller.js
@@ -5,28 +5,54 @@ FileBrowserController = function() {
 
 FileBrowserController.prototype.selectRootFolder = function(folder) {
   $("path").value = folder.nativePath;
-  $('list').innerHTML = ""
-   this.selectFolder(folder);
+  $('fileList').innerHTML = ""
+  $('directoryList').innerHTML = ""
+  this.selectFolder(folder);
 }
 
 FileBrowserController.prototype.selectFolder = function(folder, root) {
-  var files = folder.getDirectoryListing();          
-  var list = $('list');
-  var br = new Element('br');
+  var files = folder.getDirectoryListing(); 
+  var totalSize = this.directorySize(folder)         
+  var dirList = $('directoryList');
+  var fileList = $('fileList');
+
+  var fileRef = new Element('a', { rel: folder.nativePath+"/..", 'class': 'folder' } ).update("..");
+  dirList.insert(fileRef);
+  dirList.insert(new Element('br'));
+  Event.observe(fileRef, "click", this.labelClicked.bind(this));
+
   for (var i = 0; i < files.length; i++) {
-    var cssclass = files[i].isDirectory ? "folder" : "file";
-    var content = new Element('div', { name: "file", 'class': cssclass} );
-    content.insert(files[i].name);
-    content.data = files[i];
-    Event.observe(content, "click", this.labelClicked.bind(this));
-    list.insert(content);
-    list.insert(br);
+	if (files[i].isDirectory) {
+	  	var fileRef = new Element('a', { rel: files[i].nativePath, 'class': 'folder' } ).update(files[i].name);
+	    dirList.insert(fileRef);
+	    dirList.insert(new Element('br'));
+	    Event.observe(fileRef, "click", this.labelClicked.bind(this));
+	}
+  }
+
+  var content = new Element('div', { name: "fileRow1" } );
+  fileList.insert(content);
+  for (var i = 0; i < files.length; i++) {
+	if (!files[i].isDirectory) {
+		var style = this.styleForSize(files[i], totalSize);
+		var fileRef = new Element('a', { rel: files[i].nativePath, 'class': style}).update(files[i].name)
+		content.insert(fileRef);
+		content.insert(" ");
+		Event.observe(fileRef, "click", this.labelClicked.bind(this));
+		if (i > 0 && (i % 6) == 0) {
+		  var content = new Element('div', { name: "fileRow"+i } );
+		  fileList.insert(content);
+		}
+	}
   }
 }
 
 FileBrowserController.prototype.labelClicked = function(node) {
-  var file = node.currentTarget.data;
-  if (file.isDirectory) this.selectedFile = file;
+  var file = new air.File(node.currentTarget.rel);
+  if (file.isDirectory) {
+    this.selectedFile = file;
+	return this.openSelectedFile()
+  }
   var attributes = ['name', 'extension', 'type', 'size', 'modificationDate', 'creationDate' ];
   var description = "";
   for (var i=0; i<attributes.length; i++)
@@ -38,3 +64,26 @@ FileBrowserController.prototype.openSelectedFile = function() {
   this.selectRootFolder(this.selectedFile);
 }
 
+FileBrowserController.prototype.styleForSize = function(file, totalSize) {
+	var ratio = file.size/totalSize;
+	if (ratio <= .4) {
+		return 'little-file';
+	} else if (ratio >= .6) {
+		return 'big-file'
+	} else {
+		return 'normal-file';
+	}
+	
+}
+
+FileBrowserController.prototype.directorySize = function(dir) {
+	var files = dir.getDirectoryListing();
+    var size = 0 
+	for (var i = 0; i < files.length; i++) {
+		air.Introspector.Console.log("file size " + files[i].size)
+		if (files[i].size > 0) {
+			size += files[i].size;
+		}
+	}
+	return size;
+}
