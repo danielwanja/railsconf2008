4652a24c2680668f220ef07633d24507bba4f98d
Tony Hillerson <tony.hillerson@effectiveui.com>
2008-05-28 18:22:22 -0700

    formatting and commenting for file browser example

--- 04_HTMLFileBrowser/air/src/filebrowser_controller.js
+++ 04_HTMLFileBrowser/air/src/filebrowser_controller.js
@@ -10,17 +10,20 @@ FileBrowserController.prototype.selectRootFolder = function(folder) {
   this.selectFolder(folder);
 }
 
+// called when a folder is selected
 FileBrowserController.prototype.selectFolder = function(folder, root) {
-  var files = folder.getDirectoryListing(); 
+  var files = folder.getDirectoryListing();
   var totalSize = this.directorySize(folder)         
   var dirList = $('directoryList');
   var fileList = $('fileList');
 
+  // add a dot-dot listing for folder's parent directory
   var fileRef = new Element('a', { rel: folder.nativePath+"/..", 'class': 'folder' } ).update("..");
   dirList.insert(fileRef);
   dirList.insert(new Element('br'));
   Event.observe(fileRef, "click", this.labelClicked.bind(this));
 
+  // iterate over directories and add a link to those directories to the file list
   for (var i = 0; i < files.length; i++) {
 	if (files[i].isDirectory) {
 	  	var fileRef = new Element('a', { rel: files[i].nativePath, 'class': 'folder' } ).update(files[i].name);
@@ -30,15 +33,18 @@ FileBrowserController.prototype.selectFolder = function(folder, root) {
 	}
   }
 
+  // iterate over files in directory and place a link in the 'cloud' area
   var content = new Element('div', { name: "fileRow1" } );
   fileList.insert(content);
   for (var i = 0; i < files.length; i++) {
 	if (!files[i].isDirectory && files[i].exists) {
 		var style = this.styleForSize(files[i], totalSize);
+		// keep the file's path in the rel attribute of the anchor tag for later use
 		var fileRef = new Element('a', { rel: files[i].nativePath, 'class': style}).update(files[i].name)
 		content.insert(fileRef);
 		content.insert(" ");
 		Event.observe(fileRef, "click", this.labelClicked.bind(this));
+		// every six files start a new row
 		if (i > 0 && (i % 6) == 0) {
 		  var content = new Element('div', { name: "fileRow"+i } );
 		  fileList.insert(content);
@@ -47,12 +53,16 @@ FileBrowserController.prototype.selectFolder = function(folder, root) {
   }
 }
 
+// called when a label on screen is clicked
 FileBrowserController.prototype.labelClicked = function(node) {
+  // get the file's path, stored in the rel attribute of the anchor tag that was clicked
   var file = new air.File(node.currentTarget.rel);
+  // if the file is a directory, navigate to that directory
   if (file.isDirectory) {
     this.selectedFile = file;
 	return this.openSelectedFile()
   }
+  // otherwise display file attributes
   var attributes = ['name', 'extension', 'type', 'size', 'modificationDate', 'creationDate' ];
   var description = "";
   for (var i=0; i<attributes.length; i++)
@@ -64,11 +74,13 @@ FileBrowserController.prototype.openSelectedFile = function() {
   this.selectRootFolder(this.selectedFile);
 }
 
+// picks out a css class to use based on the relative size of the file to 
+// the total size of a directory
 FileBrowserController.prototype.styleForSize = function(file, totalSize) {
 	var ratio = file.size/totalSize;
-	if (ratio <= .4) {
+	if (ratio <= .2) {
 		return 'little-file';
-	} else if (ratio >= .6) {
+	} else if (ratio >= .8) {
 		return 'big-file'
 	} else {
 		return 'normal-file';
@@ -76,10 +88,12 @@ FileBrowserController.prototype.styleForSize = function(file, totalSize) {
 	
 }
 
+// calculates the size of all files in a directory
 FileBrowserController.prototype.directorySize = function(dir) {
 	var files = dir.getDirectoryListing();
     var size = 0 
 	for (var i = 0; i < files.length; i++) {
+		// links and possibly other files don't "exist" and cause problems for size
 		if (files[i].exists && files[i].size > 0) {
 			size += files[i].size;
 		}
